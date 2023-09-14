9457ab3efeb4b6bf03b0eacf9eadfaec727be0a6
Roman <75018677+RomanJodlowski@users.noreply.github.com>
2023-09-14 02:58:28 +0100

    Add files via upload

---
+++ activity01.py
@@ -0,0 +1,10 @@
+# Write a Python program to print
+# the result of the following operations.
+a = -5 + 8 * 6
+print(a)
+b = (55 + 9) % 9
+print(b)
+c = 20 + - 3 * 5 / 8
+print(c)
+d = 5 + 15 / 3 * 2 - 8 % 3
+print(d)
---
+++ activity02.py
@@ -0,0 +1,6 @@
+# Write a Python program that displays the result
+# of the following operations.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("The division of the first number and "
+      "\nthe second number is: ", int(num1 / num2))
